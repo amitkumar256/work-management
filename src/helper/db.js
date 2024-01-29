c9bbd1ef87b2f406ab3cfe5e6fe9e860d1803e5e
amit <amit256375@gmail.com>
2024-01-29 22:31:08 +0530

    making connection

---
+++ src/helper/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+export const connectDb = async()=>{
+
+
+try{
+    
+const {connection}= await mongoose.connect(process.env.MONGO_DB_URL,{
+    dbName: "work_manager",
+});
+
+console.log("db connected...");
+console.log(connection);
+}catch(error){
+console.log("failed to connect with datbase");
+console.log(error);
+}
+};
